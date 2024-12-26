8fe61fee07dc01b7c8690ad0fe597e4c6c8f4a89
Slavetomints <161798204+Slavetomints@users.noreply.github.com>
2024-12-25 18:26:38 -0600

    added WebScraper class to main.rb

---
+++ web-scraper/ruby/main.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# Initialized upon running the program, receives a website object upon
+# initialization and is the top level object the user interacts with.
+class WebScraper
+  def initialize; end
+end
